82beebe11b391625c646a30eb66a03746fbe2b7b
DeanItServices <dyllan@deanitservices.com>
2025-02-18 00:53:19 -0700

    Update DeanIT_Once.ps1

--- DeanIT_Once.ps1
+++ DeanIT_Once.ps1
@@ -1,14 +1,14 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-#Start-OSDCloud -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
+Start-OSDCloud -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
 
 # go to usb
-set-location "X:\"
+set-location "D:\"
 
 # copies the deploy folder to the SPAdmin desktop
 mkdir "C:\ProgramData\RUDI"
-Copy-Item "X:\OSDCloud\Config\Scripts\deploy" -Destination "C:\ProgramData\RUDI" -recurse
+Copy-Item ".\deploy" -Destination "C:\ProgramData\RUDI" -recurse
 
 #Restart from WinPE
 
